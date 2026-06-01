d62918ed539083917539462ab713235467cab749
Luizgsch <luizgsch1@gmail.com>
2026-05-31 22:22:21 -0300

    refactor: rewrite App.jsx for light landing page layout, remove old dark-mode components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import { EcommerceProvider } from './context/EcommerceContext.jsx';
-import { EcommerceDashboard } from './components/EcommerceDashboard.jsx';
+import { Header } from './components/Header.jsx';
+import { HeroSection } from './components/HeroSection.jsx';
+import { CatalogGrid } from './components/CatalogGrid.jsx';
+import { CartDrawer } from './components/CartDrawer.jsx';
+import { ProductModal } from './components/ProductModal.jsx';
+import { Toast } from './components/Toast.jsx';
+import { Footer } from './components/Footer.jsx';
 import './index.css';
+
+function AppContent() {
+  return (
+    <div className="flex flex-col min-h-screen bg-white">
+      <Header />
+      <main className="flex-1">
+        <HeroSection />
+        <CatalogGrid />
+      </main>
+      <CartDrawer />
+      <ProductModal />
+      <Toast />
+      <Footer />
+    </div>
+  );
+}
 
 export function App() {
   return (
     <EcommerceProvider>
-      <EcommerceDashboard />
+      <AppContent />
     </EcommerceProvider>
   );
 }
